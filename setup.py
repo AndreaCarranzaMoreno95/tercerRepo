8f9d5180033d6c343a1ea2e6edfec5202ad99bb3
Angeles Carranza Moreno <angeles.carranza@agrosoft.com.ec>
2025-06-12 17:44:31 -0500

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                                  # Nombre del paquete
+    version="0.1.0",                                      # Versión inicial
+    packages=find_packages(),                             # Paquetes a incluir
+    description="Un paquete pip simple de saludo",        # Breve descripción
+    author="Andrea Carranza",                             # Tu nombre
+    author_email="angeles20111@live.com",                 # Tu correo electrónico
+    url="https://github.com/AndreaCarranzaMoreno95/tercerrepo",     # URL del proyecto
+)
